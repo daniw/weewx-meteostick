da2efcdd747f8c6b44b6b9b7a4aff3e473adc5ea
daniw <dani.winz@bluewin.ch>
2020-01-18 15:21:18 +0100

    format changed to 'machine'

--- bin/user/meteostick.py
+++ bin/user/meteostick.py
@@ -446,7 +446,7 @@ class Meteostick(object):
         self.rf_threshold = absrfs * 2
         loginf('using rf sensitivity %s (-%s dB)' % (rfs, absrfs))
 
-        fmt = cfg.get('format', 'raw')
+        fmt = cfg.get('format', 'machine')
         if fmt.lower() not in ['machine', 'raw']:
             raise ValueError("unsupported format '%s'" % fmt)
         self.output_format = fmt.lower()
